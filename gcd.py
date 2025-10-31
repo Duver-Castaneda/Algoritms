0482bc0d4f76d4e62e9de3b3ad78aa194ae5e333
Duver Stiven Castañeda Garcia <duverstivencastanedagarcia@MacBook-Pro-de-Duver.local>
2025-10-30 21:03:05 -0500

    solved gcd.py

--- gcd.py
+++ gcd.py
@@ -1,36 +1,46 @@
 def gcd(a, b):
      current_gcd = 1
     
-     if a % 2 == 0 and b % 2 == 0:
-         for d in range(2, min(a, b) + 1, 2):
-             if a % d == 0 and b % d == 0:
-                 if d > current_gcd:
-                     current_gcd = d
+    #  if a % 2 == 0 and b % 2 == 0:
+    #      for d in range(2, min(a, b) + 1, 2):
+    #          if a % d == 0 and b % d == 0:
+    #              if d > current_gcd:
+    #                  current_gcd = d
              
-     if a % 2 != 0 and b % 2 != 0:
-         for d in range(3, min(a, b) + 1, 2):
-             if a % d == 0 and b % d == 0:
-                 if d > current_gcd:
-                     current_gcd = d
-     if a % 2 != 0 and b % 2 == 0 or a % 2 == 0 and b % 2 != 0:
-         if a % 5 == 0 and b % 5 == 0:
-            current_gcd = 5
+    #  if a % 2 != 0 and b % 2 != 0:
+    #      for d in range(3, min(a, b) + 1, 2):
+    #          if a % d == 0 and b % d == 0:
+    #              if d > current_gcd:
+    #                  current_gcd = d
+    #  if a % 2 != 0 and b % 2 == 0 or a % 2 == 0 and b % 2 != 0:
+    #      if a % 5 == 0 and b % 5 == 0:
+    #         current_gcd = 5
          
        
+   
 
 
 
 
+     for d in range(1, min(a,b)):
+        
+         r = int(a % b)
+         a = b
+         b = r
 
+       
+             
+         if b == 0:
+             current_gcd = a
+             break
+             
+         
 
-    #for d in range(2, min(a, b) + 1):
-       # if a % d == 0 and b % d == 0:
-           # if d > current_gcd:
-               # current_gcd = d
-
+       
+         
      return current_gcd
 
 
 if __name__ == "__main__":
-    a, b = map(int, input().split())
-    print(gcd(a, b))
+     a, b = map(int, input().split())
+     print(gcd(a, b))
